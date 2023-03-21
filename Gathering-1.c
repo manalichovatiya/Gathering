2a8c088d713642bc578f3f0dd71c134272e3824d
manalichovatiya <115995019+manalichovatiya@users.noreply.github.com>
2023-03-21 21:54:10 +0530

    Add files via upload

---
+++ Gathering-1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int fact(int a){
+    if(a<=1){
+        return 1;
+    }
+    else{
+        return a*fact(a-1);
+    }
+}
+int main(){
+    int y;
+    printf("Enter a Number:");
+    scanf("%d",&y);
+    
+    printf("factorial of the number is: %d",fact(y));
+
+  return 0;
+}
+
---
+++ Gathering-3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int length(char a[1000]){
+	printf("\nLength of the String is : %d", length);
+   return 0;
+}
+
+int main(){
+    char a[1000];
+	printf("Enter text : ");
+	gets(a);
+	int length;
+    length = strlen(a);
+ 	printf("\nLength of the String is : %d", length);
+
+	return 0;
+}
